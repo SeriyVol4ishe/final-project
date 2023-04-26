161fae2b9fb4de6e2d59be10b237c55ca80729af
seriyvol4ishe <sergii.d@helium10.com>
2023-04-26 18:46:01 +0300

    add new dbt model;

---
+++ dbt/models/aggregated/agg_crimes_by_iucr_by_month_by_primary_type.sql
@@ -0,0 +1,10 @@
+select
+    cr.year as year,
+    cr.month as month,
+    cr.primary_type,
+    sum(cr.crimes_count) as crimes_count
+from {{ ref('agg_crimes_by_iucr_by_month') }} as cr
+group by
+    year,
+    month,
+    primary_type
