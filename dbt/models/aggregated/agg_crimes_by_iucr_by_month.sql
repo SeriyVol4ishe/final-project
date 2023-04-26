d764ab59d920932e964bf7477f639ffadb82116d
seriyvol4ishe <sergii.d@helium10.com>
2023-04-26 12:13:48 +0300

    add model with aggregated by iucr crime data;

---
+++ dbt/models/aggregated/agg_crimes_by_iucr_by_month.sql
@@ -0,0 +1,24 @@
+select
+    cr.year as year,
+    coalesce(ca.community_name, 'undefined') as community_name,
+    extract(month from cr.date) as month,
+    cr.iucr,
+    iucr.is_active,
+    iucr.primary_description,
+    iucr.secondary_description,
+    count(cr.id) as crimes_count
+from {{ ref('stg_crime') }} as cr
+left join {{ ref('stg_iucr') }} as iucr
+    on cr.iucr = iucr.iucr
+left join {{ ref('stg_community_area') }} as ca
+    on cr.community_area = ca.area_num
+where ca.area_num is not null
+group by
+    year,
+    community_name,
+    month,
+    iucr,
+    is_active,
+    primary_description,
+    secondary_description
+order by crimes_count desc
--- dbt/models/crimes/stg_iucr.sql
+++ dbt/models/crimes/stg_iucr.sql
@@ -1,8 +1,8 @@
 select
     model.iucr as id,
-    model.iucr,
+    right('000'||model.iucr, 4) as iucr,
     model.primary_description,
     model.secondary_description,
-    model.active
+    model.active as is_active
 -- Alias added due to "bug": https://github.com/dbt-labs/dbt-bigquery/issues/33
 from {{ source('staging', 'iucr') }} as model
